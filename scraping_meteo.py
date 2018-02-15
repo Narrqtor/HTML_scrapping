5df8cee3e6e950b1f126f9eff631095dd15c547b
root <root@DESKTOP-PUHNSDK.localdomain>
2018-02-16 06:09:33 +1100

    Fetched locations and temperatures in France - raw display

--- scraping_meteo.py
+++ scraping_meteo.py
@@ -20,6 +20,16 @@ print(date)
 
 ### Retrieve the data of the zone you want to retrieve the weather from
 
-continent = soup.find_all('p')
-print(continent)
+france = soup.find(id="map-forecast")
+forecast_items = france.find_all(class_="tableToMap")
+text = forecast_items[0]
 
+#print(text.prettify())
+
+# Loop over the list you have and retrieve the information you are looking for.
+
+for infos in text:
+	location = text.find_all(class_="picTemps")
+	temp = text.find_all(class_="temper celsiusUnit")
+	for places, units in zip(location, temp):
+		print(places.get_text(), "il y fait ", units.get_text(), " degrés")
